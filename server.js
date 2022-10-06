0c09ebec864ec8889646da0e7e0f560f840d799d
wwartick <willwartick@gmail.com>
2022-10-06 11:54:23 -0700

    serving page data

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray){
     let personalityTraitsArray = [];
@@ -117,6 +118,23 @@ app.post('/api/animals', (req, res) => {
     res.json(req.body);
     }
 });
+
+app.get('/', (req , res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/zookeepers.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html' ));
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
